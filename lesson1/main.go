90d25802af47b25d3cfa59a5f314a20cdb009a8d
innerdev <innerdev@mail.ru>
2026-01-14 17:56:46 +0300

    lesson1: ToLittleEndian rewritten with bit operations

--- lesson1/main.go
+++ lesson1/main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func ToLittleEndian(number uint32) uint32 {
+	return ((number & 0xff000000) >> 24) |
+		((number & 0x00ff0000) >> 8) |
+		((number & 0x0000ff00) << 8) |
+		((number & 0x000000ff) << 24)
+}
+
+func ToLittleEndianFirstVersionWithPointers(number uint32) uint32 {
 	numberSize := unsafe.Sizeof(number) // but I have no clue about generics yet
 	numberPtr := unsafe.Pointer(&number)
 
@@ -25,6 +32,9 @@ func MyLittleEndianCheck() bool {
 
 func main() {
 	var source uint32 = 0xDDCCBBAA
+
+	fmt.Printf("%08x\n", source&0xf)
+	fmt.Printf("%d\n", (uint8)(unsafe.Sizeof(source&0xf))) // Still 4 bytes, but why? :thinking:
 
 	fmt.Println("Is machine Little Endian?", MyLittleEndianCheck())
 	fmt.Printf("Changing 0x%08X to 0x%08X\n", source, ToLittleEndian(source))
