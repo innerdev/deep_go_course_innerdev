49b98bd7ad171ae06a6b1bcae7d9090c599b9ddd
innerdev <innerdev@mail.ru>
2026-02-21 11:44:54 +0300

    lesson11

--- lesson11/main.go
+++ lesson11/main.go
@@ -1,21 +1,83 @@
 package main
 
-/*
-Домашнее задание №11
+import (
+	"fmt"
+	"unsafe"
+)
 
-В домашнем задании нужно реализовать функцию трассировки объектов.
-
-Трассировка объектов будет осуществляться со стеков, рекурсивно проходя все объекты в куче. Важно написать
-реализацию алгоритма так, чтобы не было повторных обходов одних и тех же объектов. Циклов не будет, проверку
-на это реализовывать не нужно. Для обхода объектов можно использовать алгоритмы DFS или BFS.
-
-Различать указатели и свободные участки памяти нужно будет при помощи значений в памяти, если значение непустое -
-значит это указатель, который указывает на какой-то другой участок память.
-*/
-
-func Trace(stacks [][]uintptr) []uintptr {
-	// need to implement
-	return nil
+func trace(ptr uintptr, result *[]uintptr, visited map[uintptr]bool) {
+	next := *(*uintptr)(unsafe.Pointer(ptr))
+	if next != 0 && !visited[next] {
+		visited[next] = true
+		*result = append(*result, next)
+		trace(next, result, visited)
+	}
 }
 
+func Trace(stacks [][]uintptr) []uintptr {
+	visited := make(map[uintptr]bool)
+	result := make([]uintptr, 0)
 
+	for _, stack := range stacks {
+		for _, ptr := range stack {
+			if ptr != 0x00 && !visited[ptr] {
+				visited[ptr] = true
+				result = append(result, ptr)
+
+				pointersChain := make([]uintptr, 0)
+				trace(ptr, &pointersChain, visited)
+
+				result = append(result, pointersChain...)
+			}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	var heapObjects = []int{
+		0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+
+	var heapPointer1 *int = &heapObjects[1]
+	var heapPointer2 *int = &heapObjects[2]
+	var heapPointer3 *int = nil
+	var heapPointer4 **int = &heapPointer3
+
+	var stacks = [][]uintptr{
+		{
+			uintptr(unsafe.Pointer(&heapPointer1)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[0])),
+			0x00, 0x00, 0x00, 0x00,
+		},
+		{
+			uintptr(unsafe.Pointer(&heapPointer2)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[1])),
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[2])),
+			uintptr(unsafe.Pointer(&heapPointer4)), 0x00, 0x00, 0x00,
+		},
+		{
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[3])),
+		},
+	}
+
+	expectedPointers := []uintptr{
+		uintptr(unsafe.Pointer(&heapPointer1)),
+		uintptr(unsafe.Pointer(&heapObjects[0])),
+		uintptr(unsafe.Pointer(&heapPointer2)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
+		uintptr(unsafe.Pointer(&heapObjects[2])),
+		uintptr(unsafe.Pointer(&heapPointer4)),
+		uintptr(unsafe.Pointer(&heapPointer3)),
+		uintptr(unsafe.Pointer(&heapObjects[3])),
+	}
+	pointers := Trace(stacks)
+
+	fmt.Println(expectedPointers)
+	fmt.Println(pointers)
+}
--- lesson11_question/main_test.go
+++ lesson11_question/main_test.go
@@ -9,6 +9,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+
+/*
+Домашнее задание №11
+
+В домашнем задании нужно реализовать функцию трассировки объектов.
+
+Трассировка объектов будет осуществляться со стеков, рекурсивно проходя все объекты в куче. Важно написать
+реализацию алгоритма так, чтобы не было повторных обходов одних и тех же объектов. Циклов не будет, проверку
+на это реализовывать не нужно. Для обхода объектов можно использовать алгоритмы DFS или BFS.
+
+Различать указатели и свободные участки памяти нужно будет при помощи значений в памяти, если значение непустое -
+значит это указатель, который указывает на какой-то другой участок память.
+*/
+
 func TestTrace(t *testing.T) {
 	var heapObjects = []int{
 		0x00, 0x00, 0x00, 0x00, 0x00,
@@ -40,20 +54,28 @@ func TestTrace(t *testing.T) {
 		},
 	}
 
+	pointers := Trace(stacks) // Try to move this line under `expectedPoiners` definition
 	expectedPointers := []uintptr{
 		uintptr(unsafe.Pointer(&heapPointer1)),
-		uintptr(unsafe.Pointer(&heapObjects[1])),
 		uintptr(unsafe.Pointer(&heapObjects[0])),
 		uintptr(unsafe.Pointer(&heapPointer2)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
 		uintptr(unsafe.Pointer(&heapObjects[2])),
 		uintptr(unsafe.Pointer(&heapPointer4)),
 		uintptr(unsafe.Pointer(&heapPointer3)),
 		uintptr(unsafe.Pointer(&heapObjects[3])),
 	}
-	pointers := Trace(stacks)
-
+	
 	fmt.Println("Expected:", expectedPointers)
-	fmt.Println("Actual  :", pointers)
+	fmt.Println("Stack 0 0:", stacks[0][0])
 
 	assert.True(t, reflect.DeepEqual(expectedPointers, pointers))
+}
+
+func Trace(stacks [][]uintptr) []uintptr {
+	fmt.Println("Stack 0 0 from Trace", stacks[0][0])
+	return []uintptr{}
+}
+
+func main() {
 }
